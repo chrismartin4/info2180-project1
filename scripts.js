e1cd5db66bd62ec42fe26c61e55a57cf41e3b253
Christopher Martin <chris.b411@gmail.com>
2020-11-01 21:03:13 -0500

    Added java script - Final

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+window.onload = function () {
+    var subscriptionbtn = document.getElementsByClassName("btn")[1];
+    subscriptionbtn.addEventListener("click", function (i) {
+        var email = document.getElementById("email").value;
+        var displaymsg = document.getElementsByClassName("message")[0];
+        if (email.length == 0) {
+            displaymsg.innerHTML = "Please enter a valid email address";
+        }
+        else if (email.length > 0) {
+            displaymsg.innerHTML = "Thank you! Your email address " + email + " has been added to our mailing list!";
+        }
+        i.preventDefault();
+    });
+}
